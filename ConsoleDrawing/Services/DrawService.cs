0c0ebd43ee00bcd25af4d5830272fd1525116366
Oleksandr <Panchenko.Sasha.nick74@gmail.com>
2022-01-21 00:15:10 +0200

    Added DrawService.cs

---
+++ ConsoleDrawing/Services/DrawService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using ConsoleDrawing.Models;
+using ConsoleDrawing.Tools;
+
+namespace ConsoleDrawing.Services;
+
+public sealed class DrawService
+{
+    private string SaveFile { get; set; }
+
+    public DrawService(string filename = "save.json")
+    {
+        SaveFile = filename;
+    }
+
+    public void SavePictureToFile(List<Shape> shapes)
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Converters = { new ShapeConverter() }
+        };
+        var stream = File.OpenWrite(SaveFile);
+        var json = JsonSerializer.Serialize(shapes, options);
+        using var streamWriter = new StreamWriter(stream);
+        streamWriter.WriteLine(json);
+    }
+
+    public List<Shape>? UploadFromFile()
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Converters = { new ShapeConverter() }
+        };
+        var stream = File.OpenRead(SaveFile);
+        using var streamReader = new StreamReader(stream);
+        var json = streamReader.ReadToEnd();
+        return JsonSerializer.Deserialize<List<Shape>>(json, options);
+    }
+}
