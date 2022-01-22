10159a1c7bfb7a564de374b05411b0cb340f0cc4
Oleksandr <Panchenko.Sasha.nick74@gmail.com>
2022-01-22 23:31:36 +0200

    Added factory for shapes

--- ConsoleDrawing/Factories/ShapeFactory.cs
+++ ConsoleDrawing/Factories/ShapeFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using ConsoleDrawing.Models;
+using ConsoleDrawing.Tools;
 
 namespace ConsoleDrawing.Factories;
 
@@ -28,8 +29,37 @@ public static class ShapeFactory
             _ => null
         };
     }
+    
+    private static Shape? CreateLine()
+    {
+        Console.WriteLine("Adding line..."); 
+        Console.WriteLine("Please, enter name:"); 
+        var name = Console.ReadLine(); 
+        
+        Console.WriteLine("Please, input X and Y first point coordinate:"); 
+        var x1 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+        var y1 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
 
-    private static Shape CreateCircle()
+        var point1 = new Point(x1, y1);
+
+        if (!ShapesValidator.PointValidate(point1))
+            return null;
+        
+        Console.WriteLine("Please, input X and Y second point coordinate:"); 
+        var x2 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+        var y2 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+        var point2 = new Point(x2, y2);
+
+        if (!ShapesValidator.PointValidate(point2))
+            return null;
+        
+        name ??= "Line";
+        Console.Clear();
+        return new Line(name, point1, point2);
+    }
+
+    private static Shape? CreateCircle()
     {
         Console.WriteLine("Adding circle..."); 
         Console.WriteLine("Please, enter name:"); 
@@ -37,7 +67,10 @@ public static class ShapeFactory
         
         Console.WriteLine("Please, input X and Y center coordinate:"); 
         var x = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+        var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+        var point = new Point(x, y);
+        if (!ShapesValidator.PointValidate(point))
+            return null;
         
         Console.WriteLine("Enter radius:"); 
         var radius = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
@@ -47,35 +80,43 @@ public static class ShapeFactory
 
         name ??= "Circle";
         Console.Clear();
-        return new Circle(name, new Point(x, y), radius, filled);
+        var circle = new Circle(name, point, radius, filled);
+
+        return !ShapesValidator.ValidateCircle(circle) ? null : circle;
     }
 
-    private static Shape CreateRectangle()
+    private static Shape? CreateRectangle()
     {
-        Console.WriteLine("Adding rectangle..."); 
-        Console.WriteLine("Please, enter name:"); 
-        var name = Console.ReadLine(); 
-        
-        Console.WriteLine("Please, input X and Y left upper point coordinate:"); 
-        var x = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        
-        Console.WriteLine("Enter height:"); 
-        var height = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        
-        Console.WriteLine("Enter width:"); 
-        var width = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+        Console.WriteLine("Adding rectangle...");
+        Console.WriteLine("Please, enter name:");
+        var name = Console.ReadLine();
 
-        
-        Console.WriteLine("If you want fill shape, enter[y]"); 
+        Console.WriteLine("Please, input X and Y left upper point coordinate:");
+        var x = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+        var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+        var point = new Point(x, y);
+        if (!ShapesValidator.PointValidate(point))
+            return null;
+
+        Console.WriteLine("Enter height:");
+        var height = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+        Console.WriteLine("Enter width:");
+        var width = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+        Console.WriteLine("If you want fill shape, enter[y]");
         var filled = IsFilled(Console.ReadLine());
 
         name ??= "Rectangle";
         Console.Clear();
-        return new Rectangle(name, new Point(x, y), height, width, filled);
+
+        var rect = new Rectangle(name, new Point(x, y), height, width, filled);
+
+        return !ShapesValidator.ValidateRectangle(rect) ? null : rect;
     }
 
-    private static Shape CreateSquare()
+    private static Shape? CreateSquare()
     {
         Console.WriteLine("Adding square..."); 
         Console.WriteLine("Please, enter name:"); 
@@ -85,6 +126,10 @@ public static class ShapeFactory
         var x = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
         var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
         
+        var point = new Point(x, y);
+        if (!ShapesValidator.PointValidate(point))
+            return null;
+        
         Console.WriteLine("Enter side:"); 
         var side = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
 
@@ -93,12 +138,15 @@ public static class ShapeFactory
 
         name ??= "Rectangle";
         Console.Clear();
-        return new Square(name, new Point(x, y), side, filled);
+
+        var square = new Square(name, new Point(x, y), side, filled);
+
+        return !ShapesValidator.ValidateRectangle(square) ? null : square;
     }
 
-    private static Shape CreateLine()
+    private static Shape? CreateTriangle() 
     {
-        Console.WriteLine("Adding line..."); 
+        Console.WriteLine("Adding triangle..."); 
         Console.WriteLine("Please, enter name:"); 
         var name = Console.ReadLine(); 
         
@@ -106,34 +154,32 @@ public static class ShapeFactory
         var x1 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
         var y1 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
         
+        var point1 = new Point(x1, y1);
+        if (!ShapesValidator.PointValidate(point1))
+            return null;
+        
         Console.WriteLine("Please, input X and Y second point coordinate:"); 
         var x2 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        var y2 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
+        var y2 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+      
+        var point2 = new Point(x2, y2);
+        if (!ShapesValidator.PointValidate(point1))
+            return null;
 
-        name ??= "Line";
-        Console.Clear();
-        return new Line(name, new Point(x1, y1), new Point(x2, y2));
-    }
-
-    private static Shape CreateTriangle() //TODO Make Triangle Builder 
-    {
-        Console.WriteLine("Adding triangle..."); 
-        Console.WriteLine("Please, enter name:"); 
-        var name = Console.ReadLine(); 
+        Console.WriteLine("Please, input X and Y third point coordinate:"); 
+        var x3 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+        var y3 = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
+      
+        var point3 = new Point(x3, y3);
+        if (!ShapesValidator.PointValidate(point3))
+            return null;
         
-        Console.WriteLine("Please, input X and Y upper point coordinate:"); 
-        var x = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        var y = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture); 
-        
-        Console.WriteLine("Enter side:"); 
-        var side = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
-
         Console.WriteLine("If you want fill shape, enter[y]"); 
         var filled = IsFilled(Console.ReadLine());
 
         name ??= "Rectangle";
         Console.Clear();
-        return new Square(name, new Point(x, y), side, filled);
+        return new Triangle(name, point1, point2, point3, filled);
     }
 
     private static bool IsFilled(string? command)
